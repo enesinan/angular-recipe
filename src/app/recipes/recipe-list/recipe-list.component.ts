c44dead43562f7bc0c2d0edd95e0bc2335346383
enes.inan <enes.inan@vbt.com.tr>
2022-12-11 13:25:21 +0300

    Added checks for  components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', "This is simply a test", "https://s23209.pcdn.co/wp-content/uploads/2022/10/220905_DD_Sheet-Pan-Chx-Stuffing_222-360x540.jpg")
   ];
@@ -14,5 +15,7 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  onRecipeSelected(recipe: Recipe){
+   this.recipeWasSelected.emit(recipe);
+  }
 }
